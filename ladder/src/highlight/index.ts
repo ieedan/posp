07a7065392883781d12735bbd0f50fd50a01f6a8
Aidan Bleser <aidanbleser35@gmail.com>
2024-10-15 15:15:17 -0500

    format and lint

--- ladder/src/highlight/index.ts
+++ ladder/src/highlight/index.ts
@@ -2,47 +2,47 @@ import type { Token } from "../tokens/index.ts";
 import color from "chalk";
 
 const terminal = (tokens: Token[]): string => {
-    let result = "";
+  let result = "";
 
-    for (const token of tokens) {
-        switch (token.typ) {
-            case "string":
-                result += color.green(token.lexeme)
-                break;
-            case "number":
-                result += color.yellow(token.lexeme)
-                break;
-            case "instruction":
-                result += color.blueBright(token.lexeme)
-                break;
-            case "tag":
-                result += color.blue(token.lexeme)
-                break;
-            case "[":
-                result += color.gray(token.lexeme)
-                break;
-            case "]":
-                result += color.gray(token.lexeme)
-                break;
-            case "(":
-                result += color.yellowBright(token.lexeme)
-                break;
-            case ")":
-                result += color.yellowBright(token.lexeme)
-                break;
-            case ";":
-                result += color.gray(token.lexeme)
-                break;
-            case ",":
-                result += color.gray(token.lexeme)
-                break;
-            case " ":
-                result += color.bgBlue(token.lexeme)
-                break;
-        } 
+  for (const token of tokens) {
+    switch (token.typ) {
+      case "string":
+        result += color.green(token.lexeme);
+        break;
+      case "number":
+        result += color.yellow(token.lexeme);
+        break;
+      case "instruction":
+        result += color.blueBright(token.lexeme);
+        break;
+      case "tag":
+        result += color.blue(token.lexeme);
+        break;
+      case "[":
+        result += color.gray(token.lexeme);
+        break;
+      case "]":
+        result += color.gray(token.lexeme);
+        break;
+      case "(":
+        result += color.yellowBright(token.lexeme);
+        break;
+      case ")":
+        result += color.yellowBright(token.lexeme);
+        break;
+      case ";":
+        result += color.gray(token.lexeme);
+        break;
+      case ",":
+        result += color.gray(token.lexeme);
+        break;
+      case " ":
+        result += color.bgBlue(token.lexeme);
+        break;
     }
+  }
 
-    return result;
-}
+  return result;
+};
 
-export { terminal }
+export { terminal };
--- ladder/src/scanner/index.ts
+++ ladder/src/scanner/index.ts
@@ -1,246 +1,251 @@
 import type { Token } from "../tokens/index.ts";
-import { isAlpha, isNumber, isValidForIdentifierBody, isValidForTagBody } from "../utils/index.ts";
+import {
+  isAlpha,
+  isNumber,
+  isValidForIdentifierBody,
+  isValidForTagBody,
+} from "../utils/index.ts";
 
 type Error = {
-	error: string;
-	startColumn: number;
-	endColumn: number;
+  error: string;
+  startColumn: number;
+  endColumn: number;
 };
 
 type Scanner = {
-    scan: (code: string) => [Token[], string[] | null] 
-}
+  scan: (code: string) => [Token[], string[] | null];
+};
 
 /** Creates a new scanner instance  */
 const newScanner = (): Scanner => {
-	const _scan = (code: string): [Token[], string[] | null] => {
-		let i = 0;
-		const tokens: Token[] = [];
-		let errors: Error[] | null = null;
-		let isParam = false;
+  const _scan = (code: string): [Token[], string[] | null] => {
+    let i = 0;
+    const tokens: Token[] = [];
+    let errors: Error[] | null = null;
+    let isParam = false;
 
-		const _error = (error: string, start: number) => {
-			const err = { error, startColumn: start, endColumn: i };
-			if (errors === null) {
-				errors = [err];
-				return;
-			}
+    const _error = (error: string, start: number) => {
+      const err = { error, startColumn: start, endColumn: i };
+      if (errors === null) {
+        errors = [err];
+        return;
+      }
 
-			errors.push(err);
-		};
+      errors.push(err);
+    };
 
-		/** Check if is at end */
-		const _isAtEnd = () => i >= code.length;
+    /** Check if is at end */
+    const _isAtEnd = () => i >= code.length;
 
-		/** Advance and return last character */
-		const _advance = (): string => {
-			const prev = code[i];
-			i++;
+    /** Advance and return last character */
+    const _advance = (): string => {
+      const prev = code[i];
+      i++;
 
-			return prev;
-		};
+      return prev;
+    };
 
-		const _consume = (match: string): number | undefined => {
-			while (!_isAtEnd()) {
-				if (_advance() === match) {
-					return i;
-				}
-			}
-		};
+    const _consume = (match: string): number | undefined => {
+      while (!_isAtEnd()) {
+        if (_advance() === match) {
+          return i;
+        }
+      }
+    };
 
-		const _peek = () => code[i];
+    const _peek = () => code[i];
 
-		/** See the next token */
-		const _peekNext = () => code[i + 1];
+    /** See the next token */
+    const _peekNext = () => code[i + 1];
 
-		while (!_isAtEnd()) {
-            // console.log(`i: ${i} - ${_peek()}`, tokens, errors);
-			switch (code[i]) {
-				case "[":
-					tokens.push({
-						typ: "[",
-						column: i,
-						lexeme: "[",
-					});
-					_advance();
-					break;
-				case "]":
-					tokens.push({
-						typ: "]",
-						column: i,
-						lexeme: "]",
-					});
-					_advance();
-					break;
-				case "(":
-					isParam = true;
-					tokens.push({
-						typ: "(",
-						column: i,
-						lexeme: "(",
-					});
-					_advance();
-					break;
-				case ")":
-					isParam = false;
-					tokens.push({
-						typ: ")",
-						column: i,
-						lexeme: ")",
-					});
-                    _advance()
-					break;
-				case ";":
-					tokens.push({
-						typ: ";",
-						column: i,
-						lexeme: ";",
-					});
-					_advance();
-					break;
-				case ",":
-					tokens.push({
-						typ: ",",
-						column: i,
-						lexeme: ",",
-					});
-					_advance();
-					break;
-                case " ":
+    while (!_isAtEnd()) {
+      // console.log(`i: ${i} - ${_peek()}`, tokens, errors);
+      switch (code[i]) {
+        case "[":
+          tokens.push({
+            typ: "[",
+            column: i,
+            lexeme: "[",
+          });
+          _advance();
+          break;
+        case "]":
+          tokens.push({
+            typ: "]",
+            column: i,
+            lexeme: "]",
+          });
+          _advance();
+          break;
+        case "(":
+          isParam = true;
+          tokens.push({
+            typ: "(",
+            column: i,
+            lexeme: "(",
+          });
+          _advance();
+          break;
+        case ")":
+          isParam = false;
+          tokens.push({
+            typ: ")",
+            column: i,
+            lexeme: ")",
+          });
+          _advance();
+          break;
+        case ";":
+          tokens.push({
+            typ: ";",
+            column: i,
+            lexeme: ";",
+          });
+          _advance();
+          break;
+        case ",":
+          tokens.push({
+            typ: ",",
+            column: i,
+            lexeme: ",",
+          });
+          _advance();
+          break;
+        case " ":
+          tokens.push({
+            typ: " ",
+            column: i,
+            lexeme: " ",
+          });
+          _advance();
+          break;
+        default:
+          if (isParam) {
+            let start = i;
+
+            while (!_isAtEnd() && _peek() != ")") {
+              if (_peek() === ",") {
+                tokens.push({
+                  typ: ",",
+                  column: start,
+                  lexeme: ",",
+                });
+
+                _advance();
+
+                start = i;
+
+                continue;
+              }
+
+              // first character
+              if (start == i) {
+                const first = _peek();
+                if (first === "'") {
+                  _advance();
+
+                  // parse expecting a string literal
+                  const end = _consume("'");
+
+                  if (end === undefined) {
+                    _error(
+                      "Unterminated string literal '''!",
+                      start,
+                    );
+                  } else {
+                    const str = code.slice(start, end);
+
                     tokens.push({
-						typ: " ",
-						column: i,
-						lexeme: " ",
-					});
-					_advance();
-					break;
-				default:
-					if (isParam) {
-						let start = i;
+                      typ: "string",
+                      column: start,
+                      lexeme: str,
+                    });
+                  }
+                } else if (isNumber(first) || first == "-") {
+                  // parse expecting a number
 
-						while (!_isAtEnd() && _peek() != ")") {
-							if (_peek() === ",") {
-								tokens.push({
-									typ: ",",
-									column: start,
-									lexeme: ",",
-								});
+                  _advance();
 
-                                _advance();
+                  while (isNumber(_peek())) {
+                    _advance();
+                  }
 
-								start = i;
+                  if (
+                    _peek() == "." &&
+                    isNumber(_peekNext())
+                  ) {
+                    _advance();
 
-								continue;
-							}
+                    while (isNumber(_peek())) {
+                      _advance();
+                    }
+                  }
 
-							// first character
-							if (start == i) {
-								const first = _peek();
-								if (first === "'") {
-                                    _advance();
-                                    
-									// parse expecting a string literal
-									const end = _consume("'");
+                  const num = code.slice(start, i);
 
-									if (end === undefined) {
-										_error(
-											"Unterminated string literal '''!",
-											start
-										);
-									} else {
-                                        const str = code.slice(start, end);
+                  tokens.push({
+                    typ: "number",
+                    column: start,
+                    lexeme: num,
+                  });
+                } else if (isAlpha(first)) {
+                  _advance();
 
-										tokens.push({
-											typ: "string",
-											column: start,
-											lexeme: str,
-										});
-									}
-								} else if (isNumber(first) || first == "-") {
-									// parse expecting a number
+                  while (isValidForTagBody(_peek())) {
+                    _advance();
+                  }
 
-									_advance();
+                  const tag = code.slice(start, i);
 
-									while (isNumber(_peek())) {
-										_advance();
-									}
+                  tokens.push({
+                    typ: "tag",
+                    column: start,
+                    lexeme: tag,
+                  });
+                } else {
+                  _error(
+                    "Invalid first character for instruction parameter.",
+                    i,
+                  );
+                  _advance();
+                }
 
-									if (
-										_peek() == "." &&
-										isNumber(_peekNext())
-									) {
-										_advance();
+                continue;
+              }
 
-										while (isNumber(_peek())) {
-											_advance();
-										}
-									}
+              _error("Unexpected token", i);
 
-									const num = code.slice(start, i);
+              _advance();
+            }
+          } else if (isAlpha(_peek())) {
+            const start = i;
+            _advance();
 
-									tokens.push({
-										typ: "number",
-										column: start,
-										lexeme: num,
-									});
-								} else if (isAlpha(first)) {
-									_advance();
+            while (isValidForIdentifierBody(_peek())) {
+              _advance();
+            }
 
-									while (isValidForTagBody(_peek())) {
-										_advance();
-									}
+            const instruction = code.slice(start, i);
 
-									const tag = code.slice(start, i);
+            tokens.push({
+              typ: "instruction",
+              column: start,
+              lexeme: instruction,
+            });
+          } else {
+            _error("Unexpected token", i);
+            _advance();
+          }
+          break;
+      }
+    }
 
-									tokens.push({
-										typ: "tag",
-										column: start,
-										lexeme: tag,
-									});
-								} else {
-									_error(
-										"Invalid first character for instruction parameter.",
-										i
-									);
-									_advance();
-								}
+    return [tokens, errors];
+  };
 
-								continue;
-							}
-
-							_error("Unexpected token", i);
-
-                            _advance()
-						}
-					} else if (isAlpha(_peek())) {
-						const start = i;
-						_advance();
-
-						while (isValidForIdentifierBody(_peek())) {
-							_advance();
-						}
-
-						const instruction = code.slice(start, i);
-
-						tokens.push({
-							typ: "instruction",
-							column: start,
-							lexeme: instruction,
-						});
-					} else {
-						_error("Unexpected token", i);
-						_advance();
-					}
-					break;
-			}
-		}
-
-		return [tokens, errors];
-	};
-
-	return {
-		scan: _scan,
-	};
+  return {
+    scan: _scan,
+  };
 };
 
-export { newScanner as new, type Error };
+export { type Error, newScanner as new };
